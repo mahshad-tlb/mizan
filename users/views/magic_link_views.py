10de216a5dcd5d60ca08c16836724cc01f049389
projects <mahshadtalebi.a@gmail.com>
2025-07-21 09:57:32 +0330

    users (auth-feature) add:creating a user model users(auth-feature) add:creat a signup and login view users (auth-feature) add:creating an EmaiForm and PhoneNumberForm and CodeVerificationForm for login with email and sms test commit users (auth-feature) add: creat a email view

---
+++ users/views/magic_link_views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.core.mail import send_mail
+
+from users.models import Users, LoginToken
+from users.forms.verification_forms import EmailForm
+import secrets
+
+
+def send_magic_link(request):
+    if request.method == "POST":
+        form = EmailForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            try:
+                user = Users.objects.get(email=email)
+            except Users.DoesNotExist:
+                messages.error(request, "کاربری با این ایمیل وجود ندارد.")
+                return render(request, "send_magic_link.html", {"form": form})
+
+            token = secrets.token_urlsafe(32)
+            LoginToken.objects.create(user=user, token=token)
+
+            link = request.build_absolute_uri(f"/magic-login/{token}/")
+
+            # ارسال ایمیل (در ترمینال چاپ می‌شود)
+            send_mail(
+                subject="ورود سریع به سایت",
+                message=f"برای ورود روی این لینک کلیک کنید:\n{link}",
+                from_email="noreply@example.com",
+                recipient_list=[email],
+                fail_silently=False
+            )
+
+            messages.success(request, "لینک ورود به ایمیل شما ارسال شد.")
+            return redirect("login")
+    else:
+        form = EmailForm()
+    return render(request, "send_magic_link.html", {"form": form})
+
+
