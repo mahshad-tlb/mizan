238ba79096b405598988ff2692523bbf5f4d9bb0
projects <mahshadtalebi.a@gmail.com>
2025-07-27 08:36:38 +0330

    setting (main) add: confguration for password

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -12,8 +12,27 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+    {
+        'NAME': 'django_pwned_passwords.validators.PwnedPasswordsValidator',  # <-- این
+    },
+]
 
 
 # Quick-start development settings - unsuitable for production
