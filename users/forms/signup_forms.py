e098c1d51bb6744775b5155d0b17a60d736e940e
projects <mahshadtalebi.a@gmail.com>
2025-07-27 08:55:47 +0330

    users (main) add: secondary password field to signup forms

--- users/forms/signup_forms.py
+++ users/forms/signup_forms.py
@@ -1,10 +1,41 @@
 from django import forms
+from django.contrib.auth.hashers import make_password
+from users.models import SecondaryPassword  # اطمینان حاصل کن این مدل در users/models.py هست
+from pwned_passwords_django.validators import PwnedPasswordsValidator
+from django.core.exceptions import ValidationError
+
+
+
+
 
 class SignupForm(forms.Form):
     username = forms.CharField(label="نام کاربری", max_length=15)
     password = forms.CharField(label="رمز عبور", widget=forms.PasswordInput)
+    secondary_password = forms.CharField(label="رمز دوم", widget=forms.PasswordInput)
     email = forms.EmailField(label="ایمیل")
     phone_number = forms.CharField(label="شماره همراه", max_length=15)
+
+    def clean_secondary_password(self):
+        password = self.cleaned_data.get("secondary_password")
+        validator = PwnedPasswordsValidator()
+        try:
+            validator.validate(password)
+        except ValidationError:
+            raise forms.ValidationError("رمز دوم ناامن است و قبلاً لو رفته! لطفاً رمز قوی‌تری وارد کنید.")
+        return password
+
+    def save(self, user):
+        """
+        این متد باید در view مربوطه فراخوانی شود،
+        بعد از ساختن کاربر اصلی
+        """
+        secondary_password = self.cleaned_data.get("secondary_password")
+        if secondary_password:
+            SecondaryPassword.objects.create(
+                user=user,
+                password=make_password(secondary_password)
+            )
+
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=15, label="نام کاربری")
