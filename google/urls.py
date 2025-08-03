bf13839a1f912a449d82e3080eedf92669450526
projects <mahshadtalebi.a@gmail.com>
2025-08-03 15:36:29 +0330

    google (main) fix: change url

--- google/urls.py
+++ google/urls.py
@@ -4,5 +4,5 @@ from .views import google_login_init_view, google_callback_view
 
 urlpatterns = [
     path("login/", google_login_init_view, name="google-login-init"),
-    path("callback/", google_callback_view, name="google-callback"),
+    path("login/callback/", google_callback_view, name="google-callback"),
 ]
