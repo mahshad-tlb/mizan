e0ec6bf8c5adf479587e6de5dc8e6a2dd66b974d
projects <mahshadtalebi.a@gmail.com>
2025-08-03 13:11:06 +0330

    google (main) fix

--- google/urls.py
+++ google/urls.py
@@ -1,8 +1,8 @@
-# urls.py
+# google/urls.py
 from django.urls import path
 from .views import google_login_init_view, google_callback_view
 
 urlpatterns = [
-    path("auth/google/login/", google_login_init_view, name="google-login-init"),
-    path("auth/google/callback/", google_callback_view, name="google-callback"),
+    path("login/", google_login_init_view, name="google-login-init"),
+    path("callback/", google_callback_view, name="google-callback"),
 ]
