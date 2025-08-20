5b9b64bc729402289c9c755b1e692b7fdd4eef94
projects <mahshadtalebi.a@gmail.com>
2025-08-20 08:56:08 +0330

    .

--- users/middleware/rate_limit_middleware.py
+++ users/middleware/rate_limit_middleware.py
@@ -5,33 +5,27 @@ from django.http import JsonResponse
 class RedisRateLimitMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
-        self.rate_limit = 1000
+        self.rate_limit = 10
         self.rate_limit_duration = 300
         self.block_duration = 120
         self.block_allowance = 2
-
     def __call__(self, request):
-        # اگر ریت لیمیت غیرفعال است، مستقیم ادامه بده
         if not getattr(settings, 'RATE_LIMIT_ENABLED', False):
             return self.get_response(request)
-
         ip = self.get_ip_address(request)
         count_key = f"rate_limit:{ip}"
         block_key = f"block:{ip}"
-
         block_data = cache.get(block_key)
         if block_data:
             block_count = block_data.get('count', 0)
             if block_count >= self.block_allowance:
                 return JsonResponse(
                     {"error": "شما به مدت 2 دقیقه مسدود شده‌اید. لطفاً بعداً تلاش کنید."},
-                    status=429
-                )
+                    status=429)
             else:
                 block_data['count'] = block_count + 1
                 cache.set(block_key, block_data, timeout=self.block_duration)
                 return self.get_response(request)
-
         count = cache.get(count_key)
         if count is None:
             cache.set(count_key, 1, timeout=self.rate_limit_duration)
@@ -43,9 +37,7 @@ class RedisRateLimitMiddleware:
             )
         else:
             cache.incr(count_key)
-
         return self.get_response(request)
-
     def get_ip_address(self, request):
         x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
         if x_forwarded_for:
